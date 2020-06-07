2101ad04c325235340a9d084cac283f2240454a0
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 14:48:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(array)# code an argument here
   i = 0
   while i < array.length
-    puts "#{i = 1}. #{array[i]}"
+    puts "#{i + 1}. #{array[i]}"
     i = i += 1
     end
 end
@@ -15,10 +15,10 @@ roll_call_dwarves(dwarves)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def summon_captain_planet(calls)
+def summon_captain_planet(array)
   i = 0
   while i < array.length
-    new_array << array[i].capitalize + "!"
+    new_array << array[i].upcase + "!"
     i += 1
   end
   new_array
@@ -27,7 +27,7 @@ summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(calls)
   i = 0
-  if array.any? {|i| i.length > 4}
+  if array.any? {|i|} i.length > 4
     return true
   else
     return false
@@ -36,10 +36,9 @@ def long_planeteer_calls(calls)
 end
 long_planeteer_calls(planeteer_calls)
 
-
+#cheese_types = "cheddar", "gouda"`, and
+#"camembert"
 #cheese_types = "cheddar", "gouda"`, and
 #"camembert"
 #ingredients = ["garlic", "rosemary", "bread"]
 def find_the_cheese(array)
-cheese_types.include?(type)
-end
